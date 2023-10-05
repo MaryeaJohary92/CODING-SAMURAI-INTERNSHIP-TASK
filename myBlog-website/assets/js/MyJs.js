8d00ce712589f487bc27ee0c134bb951de1b77ca
MaryeaJohary92 <96393443+MaryeaJohary92@users.noreply.github.com>
2023-10-05 21:34:50 +0300

    Add files via upload

---
+++ myBlog-website/assets/js/MyJs.js
@@ -0,0 +1,14 @@
+function myFunctionToHide() {
+    var x = document.getElementsByClassName("city");
+    for (var i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+  }
+  
+  function myFunction() {
+    alert("The Link Is Invalid!");
+  }
+  
+  function onSubmit(){
+    alert('Your Data Has Been Submitted!, Now I will Reach For You');
+  }
